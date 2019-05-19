21e05539b070944bc5818b1b2840df52e39451d4
Lam Fuong <lamfuong@Lams-MacBook-Air.local>
2019-05-19 21:22:02 +0700

    making express progress

--- pages/a.tsx
+++ pages/a.tsx
@@ -1,3 +1,22 @@
 import React from 'react'
+import fetch from 'isomorphic-unfetch'
 
-export default () => <div>a</div>
+const A = (props:any) => (
+  <div>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium} />
+  </div>
+)
+
+A.getInitialProps = async function(context: any) {
+  const { id } = context.query
+  const res = await fetch(`http://localhost:3000/a`)
+  const show = await res.json()
+
+  console.log(`Fetched show: ${show.name}`)
+
+  return { show }
+}
+
+export default A
--- server/routes/a.ts
+++ server/routes/a.ts
@@ -1,9 +1,22 @@
+import * as fetchImport from 'isomorphic-unfetch'
+const fetch = (fetchImport.default || fetchImport) as typeof fetchImport.default
+const credentials = {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+}
 export class aRoutes {
 
     public routes(app: any, server: any): void {
+        const actualPage = '/a'
+        const url = 'https://graph.facebook.com/search?type=adinterestsuggestion&interest_list=[%22Golf%22]&limit=1000&locale=en_US&access_token=https://graph.facebook.com/search?type=adinterest&q=Golf&limit=10000&locale=en_US&access_token=EAAHR94fSmuMBAJCnBfpij6XuhZBZCWY5WSis3AhippkwtLwj9bNqqI37JfCNGhYs3aaE7Hf2v43GojK6K6vMevRuVX1ceOubwqLMPcc9JkJUyJvg30NlpCamRXOh7kURTqRZCmNOpJwjRKx7cpJbgt1aK7gPttwcu02syihau7QgPshrcRCQQBsNnfS984ZD';
         server.get('/a', (req: any, res: any) => {
-            const actualPage = '/a'
-            app.render(req, res, actualPage, req.query)
+            const data = fetch(url, credentials)
+            .then((response: any) => response.json())
+            .then((data: any) => data)
+            .catch((error: any) => console.log(`error ${error}`));
+            return app.render(req, res, actualPage, { data })
         });
     }
 
