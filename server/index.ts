eb5d7aec4f9fe55685af4c88e92bd4d47fe8d7a3
Lam Fuong <lamfuong@Lams-MacBook-Air.local>
2019-05-19 11:59:18 +0700

    Adding express

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,35 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import * as next from 'next'
+const express = require('express')
+const next = require('next')
 
-const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare()
-.then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    const { pathname, query } = parsedUrl
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+  
+    server.get('/a', (req: any, res: any) => {
+      const actualPage = '/a'
+      app.render(req, res, actualPage, req.query)
+    })
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
+    server.get('/b', (req: any, res: any) => {
+      const actualPage = '/b'
+      app.render(req, res, actualPage, req.query)
+    })
+
+    server.get('*', (req: any, res: any) => {
+      return handle(req, res)
+    })
+
+    server.listen(3000, (err: any) => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
   })
-  .listen(port)
-})
+  .catch((ex: any) => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
